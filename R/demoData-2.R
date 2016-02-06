1b80cb119af715aa8041b97760443edc98207bf4
Avnit Bambah <abambah@gmail.com>
2016-02-06 03:50:33 +0000

    Breaking the program into sub sections

--- R/demoData-2.R
+++ R/demoData-2.R
@@ -1,60 +1,55 @@
-
-#install.packages("quantstrat", repos="http://R-Forge.R-project.org")
 require(TTR)
 require(quantmod)
 require(PerformanceAnalytics)
 require(quantstrat)
 
-#suppress warings
-options("getSymbols.warning4.0" = FALSE)
-
-#Do some house cleaning
-#rm(list = ls(.blotter),envir = .blotter)
-
-#set Currency and timezone
-currency('USD')
-Sys.setenv(TZ = "UTC")
+#Required in windows . Hash this in Mac
+#install.packages("curl")
+library(curl)
 
 
-# we need a config file here to get the dates and symbols from the file
-# Start Date | End Date | Name of the file
-# But in the book this is not done
-# for now i am just plugging in values
+# price of sugar
+#install.packages("devtools")
+library(devtools)
+#install_github('quandl/R-package')
+library(Quandl)
 
-# MyData <- read.csv(file = "holdings-xlk.csv", header = TRUE, sep = ",")
-symbols <- c("HSY","MDLZ",
-"RMCF",
-"TR" )
+options("getsymbols.warning4.0" = FALSE)
 
 initDate = "2001-01-01"
-from="2013-01-01"
 
+# In sample
+from="2013-01-01"
 to = "2016-01-01"
 
-# download all the data
-if ( !"AAPL" %in% ls())
+rm(list = ls(.blotter), envir = .blotter)
+
+
+currency('USD')
+
+Sys.setenv(TZ = "UTC")
+
+# this come from Xls sheet and hard coded
+stock.str <-  c("HSY","MDLZ", "RMCF", "TR","KO","PEP","SODA","DPS","CAG","GIS","SBUX","KHC",
+                "SJM","CCE","UN","DF","SYUT","DNKN","MCD","RRGB","TSN","CPB","TR","HRL","MKC",
+                "LANC","BGS","LNCE","JBSS","PF","KKD","SONC","JACK","WEN","JMBA","MNST","FIZZ",
+                "JJSF","INGR","RMCF","WWAV","FDP","YUM","CBO","DENN","POST"
+)
+# Loop only if we need to download the data.
+if (!"HSY" %in% ls())
 {
   #if data is not in the enviornment then get it from Yahoo getSymbol
   suppressMessages(
     getSymbols(
-      symbols,from = from, to = to,src = "google"
+      stock.str,from = from, to = to, index.class = c('POSIXt','POSIXct'),env =
+        globalenv(),src = "google"
     )
   )
+
 }
 
-stock(symbols,currency = "USD",multiplier = 1)
-
-# get unemployment number from the file
-# again here should be in config file
-#
-
-#unEmploymentData <- read.csv("C:/Users/ptemprom/Desktop/SeriesReportUnemployment.csv",header = TRUE,sep = ",")
-
-# unEmploymentData[9,2] Jan 2013
-# need to change names of the columns and rows in dataFrame.
-
-
-#ChangeInUnEmployment<-diff(log(t(unEmploymentData)[seq(1,13),seq(1,11)]))
+# get sugar price as dataframe
+as.data.frame(Quandl("FRED/M04031US35620M267NNBR")) -> SugarPrices
 
 
 
