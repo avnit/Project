c91104666174377d7a82da2d2a29f4ac46c884f2
Avnit Bambah <abambah@gmail.com>
2016-02-09 22:06:08 -0600

    Add these changes

--- R/InSampleTesting.R
+++ R/InSampleTesting.R
@@ -8,8 +8,8 @@ to = "2016-01-01"
 #decisions
 BuyChange<-0.1
 sellChange<--0.2
-buyRSi<-50
-sellRsi<-20
+buyRSi<-70
+sellRsi<-30
 BuyCci<-60
 SellCci<-20
 buyBbanbs<-0.7
@@ -17,9 +17,9 @@ sellBbands<-0.3
 thresholdVol <- 0
 initEq = 50000
 
-# initialize the portfolio and download data 
+# initialize the portfolio and download data
 source('~/Project/R/initialize.R')
-# initial the functions that are required for quant start 
+# initial the functions that are required for quant start
 source('~/Project/R/functions.R')
 # call quant start and get all the data into env
 source('~/Project/R/ProjectStart.R')
